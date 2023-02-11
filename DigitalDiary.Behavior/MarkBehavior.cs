e3adb8a99f8ba5bf84447cb798946fc55aae6865
sleeme2020 <sleeme2020@yandex.ru>
2023-02-11 16:08:44 +0300

    правка

--- DigitalDiary.Behavior/MarkBehavior.cs
+++ DigitalDiary.Behavior/MarkBehavior.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DigitalDiary.Behavior.DataBase;
+using DigitalDiary.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,35 @@ using System.Threading.Tasks;
 
 namespace DigitalDiary.Behavior
 {
-    internal class MarkBehavior
+    public static class MarkBehavior
     {
+        public static Mark[] GetMarks(Human human)
+        {
+            using (AppDB appDB = new AppDB())
+            {
+                IEnumerable<Mark> marks;
+                switch(human)
+                {
+                    case Parent parent:                        
+                        marks= appDB.Marks.Where(u=>appDB.Students.Any(s => s.Parent == parent));
+
+                        break;
+
+                    case Student student:
+                        marks = appDB.Marks.Where(u => u.Student==student);
+
+                        break;
+
+                    case Teacher teacher:
+                        marks = appDB.Marks.Where(u=>u.Teacher==teacher);
+                        break;
+                    default:
+                        marks= new List<Mark>();
+                        break;
+                }
+
+                return marks.ToArray(); 
+            }
+        }
     }
 }
