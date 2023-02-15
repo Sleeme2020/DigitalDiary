87fb5e65dc7c8961b5a3aaa9a78f3fe09fbb0ef6
sleeme2020@yandex.ru <sleeme2020@yandex.ru>
2023-02-15 19:53:58 +0300

    classwoork 15/02/2023

--- DigitalDiary.App/Pages/UserFrame/UserFrameInfo.xaml.cs
+++ DigitalDiary.App/Pages/UserFrame/UserFrameInfo.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using DigitalDiary.App.Infrastructure;
+using DigitalDiary.Model;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +22,21 @@ namespace DigitalDiary.App.Pages.UserFrame
     /// <summary>
     /// Логика взаимодействия для UserFrameInfo.xaml
     /// </summary>
-    public partial class UserFrameInfo : Page
+    public partial class UserFrameInfo : Page, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
         public UserFrameInfo()
         {
             InitializeComponent();
+            DataContext = this;
+            User = SingleTon.AuthUser;
         }
+
+        public User User { get; set; }
     }
 }
--- DigitalDiary.App/VM/RegistryViewModel.cs
+++ DigitalDiary.App/VM/RegistryViewModel.cs
@@ -3,6 +3,7 @@ using DigitalDiary.Behavior;
 using DigitalDiary.Model;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +13,20 @@ namespace DigitalDiary.App.VM
 {
     internal class RegistryViewModel
     {
+        
         Window _window;
         public RegistryViewModel(Window window) 
         {
             User = new User();
-            _window= window; 
+            _window= window;
+            UserTypes = new();
+            UserTypes.Add(TypeUser.Student);
+            UserTypes.Add(TypeUser.Teacher);
+            UserTypes.Add(TypeUser.Parent);
         }
         public User User { get; set; }
 
+        public ObservableCollection<TypeUser> UserTypes { get; set; }
 
         RelayCommand? _Quest;
         public RelayCommand Quest
