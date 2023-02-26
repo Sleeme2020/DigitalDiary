b5b8dcdc8ef839ce8b1f16a31adf39fee5539047
TereshkinIlya <ilya_tereshkin@bk.ru>
2023-02-26 13:42:02 +0300

    добавлено поведение

--- DigitalDiary.Behavior/MarkBehavior.cs
+++ DigitalDiary.Behavior/MarkBehavior.cs
@@ -11,33 +11,131 @@ namespace DigitalDiary.Behavior
 {
     public static class MarkBehavior
     {
-        public static Mark[] GetMarks(Human human)
+        private static IEnumerable<Mark> DefinePerson(Human human)
         {
             using (AppDB appDB = new AppDB())
             {
                 IEnumerable<Mark> marks;
-                switch(human)
+                switch (human)
                 {
-                    case Parent parent:                        
-                        marks= appDB.Marks.Where(u=>appDB.Students.Any(s => s.Parent == parent));
+                    case Parent parent:
+                        marks = appDB.Marks.Where(u => appDB.Students.Any(s => s.Parent == parent));
 
                         break;
 
                     case Student student:
-                        marks = appDB.Marks.Where(u => u.Student==student);
+                        marks = appDB.Marks.Where(u => u.Student == student);
 
                         break;
 
                     case Teacher teacher:
-                        marks = appDB.Marks.Where(u=>u.Teacher==teacher);
+                        marks = appDB.Marks.Where(u => u.Teacher == teacher);
                         break;
                     default:
-                        marks= new List<Mark>();
+                        marks = new List<Mark>();
                         break;
                 }
-
-                return marks.ToArray(); 
+                return marks;
             }
+        }
+        public static Mark[] GetMarks(Human human)
+        {
+            return DefinePerson(human).ToArray();
+        }
+        public static void DeleteMark(Human human, Work work)
+        {
+            IEnumerable<Mark> marks = DefinePerson(human);
+
+            Mark mark = marks.FirstOrDefault(w => w.Id == work.Id);
+
+            if (mark != null)
+            {
+                using (AppDB appDB = new AppDB())
+                {
+                    appDB.Remove(mark);
+                    appDB.SaveChanges();
+                }
+            }
+
+        }
+        public static void DeleteMark(Human human, Work[] works)
+        {
+            IEnumerable<Mark> marks = DefinePerson(human);
+
+            for (int i = 0; i < marks.Count(); i++)
+            {
+                Mark? mark = marks.FirstOrDefault(w => w.Id == works[i].Id);
+
+                if (mark != null)
+                {
+                    using (AppDB appDB = new AppDB())
+                    {
+                        appDB.Remove(marks);
+                        appDB.SaveChanges();
+                    }
+                }
+            }
+        }
+
+        public static void DeleteMark(int Id)
+        {
+            using (AppDB appDB = new AppDB())
+            {
+                Mark mark = appDB.Marks.FirstOrDefault(m => m.Id == Id);
+
+                if (mark != null)
+                    appDB.Remove(mark);
+
+                appDB.SaveChanges();
+            }
+        }
+
+        public static void PostMark(Human human, Work work, int mark_val)
+        {
+            if (mark_val <= 0 && mark_val > 12)
+                throw new Exception("некорректная оценка");
+
+            if (human is Student)
+            {
+                using (AppDB appDB = new AppDB())
+                {
+                    Mark? mark = appDB.Marks.FirstOrDefault(m => m.Student.Id == human.Id
+                                                           && m.WorkId == work.Id);
+                    if (mark != null)
+                    {
+                        mark.Val = mark_val;
+                        appDB.Update(mark);
+                        appDB.SaveChanges();
+                    }
+                }
+            }
+            else
+                throw new ArgumentException("не студент");
+        }
+        public static void PostMark(Human human, Mark new_mark)
+        {
+            IEnumerable<Mark> marks;
+
+            if (human is Student)
+            {
+                using (AppDB appDB = new AppDB())
+                    marks = appDB.Marks.Where(s => s.Student == human);
+
+                Mark mark = marks.FirstOrDefault(m => m.Id == new_mark.Id);
+
+                if (mark != null)
+                {
+                    mark = new_mark;
+
+                    using (AppDB appDB = new AppDB())
+                    {
+                        appDB.Update(mark);
+                        appDB.SaveChanges();
+                    }
+                }
+            }
+            else
+                throw new ArgumentException("не студент");
         }
     }
 }
---
+++ DigitalDiary.Behavior/WorkBehaviour.cs
@@ -0,0 +1,106 @@
+﻿using DigitalDiary.Behavior.DataBase;
+using DigitalDiary.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalDiary.Behavior
+{
+    public static class WorkBehaviour
+    {
+        public static void PostPresence(Student student, DateTime date, bool val)
+        {
+            if (student == null) throw new ArgumentNullException();
+
+            using (AppDB appDB = new AppDB())
+            {
+                var pres = appDB.Presences.FirstOrDefault(s => s.Id == student.Id
+                                                            && s.ClassWork.Date == date);
+                if (pres != null)
+                {
+                    pres.Val = val;
+                    appDB.Update(pres);
+                }
+                appDB.SaveChanges();
+            }
+        }
+
+        public static void PostPresence(Student[] student, DateTime date, bool[] val)
+        {
+            if (student == null || val == null) throw new ArgumentNullException();
+
+            Presence[] presences = new Presence[student.Length];
+
+            using (AppDB appDB = new AppDB())
+            {
+                for (int i = 0; i < student.Length; i++)
+                {
+                    presences[i] = appDB.Presences.FirstOrDefault(s => s.Id == student[i].Id
+                                                                    && s.ClassWork.Date == date);
+
+                    if (presences[i] != null)
+                    {
+                        presences[i].Val = val[i];
+                        appDB.Update(presences[i]);
+                    }
+                }
+                appDB.SaveChanges();
+            }
+        }
+        public static void PostClassWork(PairWork pairWork)
+        {
+            ClassWork classWork = new ClassWork()
+            {
+                PairWork = pairWork
+            };
+            using (AppDB appDB = new AppDB())
+            {
+                appDB.Add(classWork);
+                appDB.SaveChanges();
+            }
+        }
+
+        public static void PostPairWork(DateTime StartDate, DateTime EndDate, string Name)
+        {
+            PairWork pairWork = new PairWork()
+            {
+                DateStart = StartDate,
+                DateEnd = EndDate,
+                Name = Name
+            };
+            using (AppDB appDB = new AppDB())
+            {
+                appDB.Add(pairWork);
+                appDB.SaveChanges();
+            }
+        }
+
+        public static void PostHomeWork(HomeWork homeWork)
+        {
+            if (homeWork == null) throw new ArgumentNullException();
+
+            ClassWork classWork;
+
+            using (AppDB appDB = new AppDB())
+            {
+                classWork = appDB.ClassWorks.FirstOrDefault(c => c.Id == homeWork.ClassWorkId);
+
+                classWork?.HomeWorks.Add(homeWork);
+
+                appDB.Update(classWork);
+                appDB.SaveChanges();
+            }
+        }
+        public static Student[] GetStudentsFromGroup(Group group)
+        {
+            Student[] students;
+
+            using (AppDB appDB = new AppDB())
+                students = appDB.Groups.FirstOrDefault(g => g.Id == group.Id).Students.ToArray();
+
+            return students;
+        }
+    }
+}
